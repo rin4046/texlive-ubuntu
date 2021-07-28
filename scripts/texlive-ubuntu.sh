3db87341cadf8c0fdf06da5548b2f9c513db44aa
rin4046 <rin@rinrin.me>
2021-07-29 03:40:13 +0900

    update shell scripts

--- scripts/texlive-ubuntu.sh
+++ scripts/texlive-ubuntu.sh
@@ -1,24 +1,9 @@
 #!/bin/bash
 
-task=$1
-dir=$2
-outdir=$3
-docfile=$4
-doc_ext=$5
+set -e
 
-function docker_run() {
-  docker run --rm \
-    --mount "type=bind,source=${HOME}/.latexmkrc,target=/root/.latexmkrc,readonly" \
-    --mount "type=bind,source=${dir},target=/latex-workdir" \
-    --mount "type=volume,source=latex-cache,target=/latex-cache" \
-    rin4046/texlive-ubuntu "${@}"
-}
-
-case "${task}" in
-  "latexmk" )
-    docker_run "latexmk" "-silent" "-outdir=${outdir}" "${docfile}"
-    docker_run "fix-synctex" "${doc_ext}" "${outdir}" ;;
-
-  "latexindent" )
-    docker_run "latexindent" "__latexindent_temp.tex" "-y=defaultIndent: '  '" ;;
-esac
+docker run --rm \
+  --mount "type=bind,source=${HOME}/.latexmkrc,target=/root/.latexmkrc,readonly" \
+  --mount "type=bind,source=${1},target=/latex-workdir" \
+  --mount "type=volume,source=latex-cache,target=/latex-cache" \
+  rin4046/texlive-ubuntu bash -c "${2}"
