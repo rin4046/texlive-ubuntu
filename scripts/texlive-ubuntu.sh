f24a6542c711402dcb2269551fd7b2a5933f8c91
rin4046 <rin@rinrin.me>
2021-07-31 08:43:45 +0900

    update scripts

--- scripts/texlive-ubuntu.sh
+++ scripts/texlive-ubuntu.sh
@@ -2,8 +2,11 @@
 
 set -e
 
+dir="${1}"
+command="${2}"
+
 docker run --rm \
   --mount "type=bind,source=${HOME}/.latexmkrc,target=/root/.latexmkrc,readonly" \
-  --mount "type=bind,source=${1},target=/latex-workdir" \
+  --mount "type=bind,source=${dir},target=/latex-workdir" \
   --mount "type=volume,source=latex-cache,target=/latex-cache" \
-  rin4046/texlive-ubuntu:latest bash -c "${2}"
+  rin4046/texlive-ubuntu:latest bash -c "${command}"
