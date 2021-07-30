d2b518072e9b955632db60792c57cba96e1d3c61
rin4046 <rin@rinrin.me>
2021-07-31 02:13:48 +0900

    update scripts

--- scripts/texlive-ubuntu.ps1
+++ scripts/texlive-ubuntu.ps1
@@ -10,4 +10,4 @@ docker run --rm `
   --mount "type=bind,source=$(Get-PosixPwd)/.latexmkrc,target=/root/.latexmkrc,readonly" `
   --mount "type=bind,source=${Dir},target=/latex-workdir" `
   --mount "type=volume,source=latex-cache,target=/latex-cache" `
-  rin4046/texlive-ubuntu bash -c "${Commands}"
+  rin4046/texlive-ubuntu:latest bash -c "${Commands}"
--- scripts/texlive-ubuntu.sh
+++ scripts/texlive-ubuntu.sh
@@ -6,4 +6,4 @@ docker run --rm \
   --mount "type=bind,source=${HOME}/.latexmkrc,target=/root/.latexmkrc,readonly" \
   --mount "type=bind,source=${1},target=/latex-workdir" \
   --mount "type=volume,source=latex-cache,target=/latex-cache" \
-  rin4046/texlive-ubuntu bash -c "${2}"
+  rin4046/texlive-ubuntu:latest bash -c "${2}"
